bcc32b6d3605c69051b8ddac13db5b8f75001930
Daniel Laloush <d.laloush@outlook.com>
2020-04-10 10:35:48 -0400

    testing linters

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/** Array of Books  */
 const myLibrary = [];
 
 /** Generate an Empty Book  */
@@ -8,12 +9,13 @@ function Book() {
   this.isReaded = 'on';
 }
 
-
+/** Render all Array Elements */
 function render() {
   let counter = 1;
   myLibrary.forEach(obj => {
     const mainContainer = document.createElement('div');
     mainContainer.setAttribute('name', `book-${counter}`);
+    mainContainer.setAttribute('class', 'book');
     Object.values(obj).forEach(value => {
       const spanValue = document.createElement('span');
       spanValue.innerText = value;
@@ -30,18 +32,20 @@ function seeds() {
   b1.author = 'William Walker Atkinson';
   b1.title = 'Le Kybalion';
   b1.numberOfPages = 233;
-  b1.isReaded = 'on';
+  b1.isReaded = true;
   myLibrary.push(b1);
 
   b1 = new Book();
   b1.author = 'Charles Webster Leadbeater';
   b1.title = 'Occult Chemistry';
   b1.numberOfPages = 114;
-  b1.isReaded = 'on';
+  b1.isReaded = true;
   myLibrary.push(b1);
 }
 
 
+
+/** On Load Properties  */
 window.onload = () => {
   seeds();
   render();
