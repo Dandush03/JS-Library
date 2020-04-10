006b4e1a6775bd05a81754faa0a1e7c79df70e3a
Daniel Laloush <d.laloush@outlook.com>
2020-04-10 18:57:10 -0400

    fixing linter errors

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 /** Array of Books  */
 //localStorage.removeItem('library')
 let myLibrary
+
+/** Generate an Empty Book  */
+function Book() {
+  this.id = 1;
+  this.author = 'localLibrary';
+  this.title = 'localLibrary';
+  this.pages = 123;
+  this.isReaded = 'Pending';
+}
+
 localLibrary = localStorage.getItem('library');
 if (localLibrary === null) {
   /** Generate first 2 values of DB  */
@@ -23,18 +33,9 @@ if (localLibrary === null) {
   }
   myLibrary = [];
   seeds();
-  localStorage.setItem('library', JSON.stringify(myLibrary))
+  localStorage.setItem('library', JSON.stringify(myLibrary));
 } else {
   myLibrary = JSON.parse(localStorage.getItem('library'));
-}
-
-/** Generate an Empty Book  */
-function Book() {
-  this.id = 1;
-  this.author = 'localLibrary';
-  this.title = 'localLibrary';
-  this.pages = 123;
-  this.isReaded = 'Pending';
 }
 
 /** Changes Book Status */
@@ -54,7 +55,7 @@ function changeStatus(e) {
         target.innerText = 'Readed';
       }
     }
-    localStorage.setItem('library', JSON.stringify(myLibrary))
+    localStorage.setItem('library', JSON.stringify(myLibrary));
   });
 }
 
@@ -100,11 +101,11 @@ function addBookToLibrary() {
     if (key !== 'isReaded' && key !== 'id') {
       book[key] = inputs[key].value;
     } else if (key === 'id') {
-      book[key] = myLibrary.length + 1
+      book[key] = myLibrary.length + 1;
     }
   });
-  myLibrary.push(book)
-  localStorage.setItem('library', JSON.stringify(myLibrary))
+  myLibrary.push(book);
+  localStorage.setItem('library', JSON.stringify(myLibrary));
 }
 
 /** Creat a form on click  */
@@ -141,7 +142,7 @@ function formCreator() {
     const submitDiv = document.createElement('div');
     const frmSubmit = document.createElement('button');
     frmSubmit.innerText = 'Submit';
-    form.onsubmit = addBookToLibrary
+    form.onsubmit = addBookToLibrary,
     submitDiv.appendChild(frmSubmit);
     form.appendChild(submitDiv);
     const library = document.getElementsByClassName('container');
